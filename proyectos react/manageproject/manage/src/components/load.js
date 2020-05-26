0a38068aa14e99d60e76bc84111d83918a56a846
Circuandante <circuandante@gmail.com>
2020-05-25 23:12:23 -0500

    project manager: add sass al proyecto

---
+++ proyectos react/manageproject/manage/src/components/load.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import '../static/css/main.css';
+
+// const Element = React.createElement('p', {}, 'hola soy un children');
+class Load extends React.Component {
+  render() {
+    return (
+      <div className="load">
+        <p>hello world</p>
+      </div>
+    )
+  }
+}
+export default Load;
