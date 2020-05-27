099053141f7a38bcdf7189d93c52e16103bf340a
Circuandante <circuandante@gmail.com>
2020-05-26 22:06:14 -0500

    creado load de carga de página

--- proyectos react/manageproject/manage/src/App.js
+++ proyectos react/manageproject/manage/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './Styles/main.scss';
 import Load from './components/load';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Load />
+        <Load load="50"/>
       </header>
     </div>
   );
--- proyectos react/manageproject/manage/src/components/load.js
+++ proyectos react/manageproject/manage/src/components/load.js
@@ -1,12 +1,43 @@
 import React from 'react';
+// import ReactCSSTransitionGroup from 'react-transition-group';
 // import '../static/css/main.css';
 
 // const Element = React.createElement('p', {}, 'hola soy un children');
 class Load extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      load : 0,
+    }
+    console.log('1')
+  }
+
+  componentDidMount() {
+    this.setState({load:this.state.load+15})
+    setTimeout(() => {
+        this.setState({load:this.state.load+20})
+    }, 1000)
+    setTimeout(() => {
+        this.setState({load:this.state.load+29})
+    }, 2000)
+    setTimeout(() => {
+        this.setState({load:this.state.load+25})
+    }, 4000)
+    setTimeout(() => {
+        this.setState({load:this.state.load+10})
+    }, 5000)
+  }
+  
   render() {
     return (
       <div className="load">
-        <p>hello world</p>
+        <p className="load--text">Cargando proyectos</p>
+        <p className="load--number">{this.state.load}%</p>
+        <div className="bar-load">
+          <div className="bar--bakground"></div>
+          <div className="bar__progress"></div>
+        </div>
       </div>
     )
   }
